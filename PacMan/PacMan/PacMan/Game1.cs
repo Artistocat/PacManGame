a5837304dbb6fdc0389c976318969fc7b98bdfed
Isa <isaiah.rubio.130@k12.friscoisd.org>
2019-01-10 12:03:57 -0600

    new commit de Isaiah

--- PacMan/PacMan/PacMan/Game1.cs
+++ PacMan/PacMan/PacMan/Game1.cs
@@ -87,5 +87,21 @@ namespace PacMan
 
             base.Draw(gameTime);
         }
+
+        public void makePellet(double a, double b, int n)
+        {
+            //At start of every round game, pellet objects are made
+            //
+            Pellet asdf = new Pellet(a,b,n);
+            //addPellettTexture here
+        }
+
+        public void addPelletTexture(Pellet myPellet)
+        {
+            
+        }
+
+
+
     }
 }
---
+++ PacMan/PacMan/PacMan/Pellet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PacMan
+{
+    class Pellet
+    {
+        Boolean isPowerPellet;
+        Boolean isEaten;
+        double x, y;
+        int pelletNum;
+        Rectangle rect;
+        Texture2D texture;
+        
+
+        /*Notes:
+         * pellet size is 6x6
+         * powerpellet size is 12x12
+         * 
+         */
+
+
+        public Pellet(double a,double b, int n)
+        {
+            x = a;
+            y = b;
+            pelletNum = n;
+            rect = new Rectangle((int)-1000,(int)-1000,6,6);
+
+        }
+
+        public void placePellet()
+        {
+            rect.X = (int)x;
+            rect.Y = (int)y;
+        }
+
+        public int MethodExample()
+        {
+            p = new Pellet(this.x,this.y);
+            return -1;
+        }
+
+
+    }
+}
