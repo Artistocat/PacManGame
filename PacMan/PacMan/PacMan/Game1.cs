3eca4dfba8519e3e29baa4aaa58ce7261eba0173
KaneStasey :) <kane.stasey.484@k12.friscoisd.org>
2019-01-16 11:35:56 -0600

    Pacboi moves

--- PacMan/PacMan/PacMan/Game1.cs
+++ PacMan/PacMan/PacMan/Game1.cs
@@ -54,7 +54,7 @@ namespace Pacman
 
             // TODO: Add your initialization logic here
             boi = new Pacboi(Content.Load<Texture2D>("spritesheet"), new Rectangle(300, 400, 45, 45),
-                new Rectangle(3, 0, 15, 15), new Vector2(0, 0));
+                new Rectangle(3, 0, 16, 16), new Vector2(0, 0));
 
             int screenWidth = graphics.GraphicsDevice.Viewport.Width;
             int screenHeight = graphics.GraphicsDevice.Viewport.Height;
@@ -109,8 +109,26 @@ namespace Pacman
 
             if (kb.IsKeyDown(Keys.A))
             {
-                boi.source.Y = 15;
+                boi.velocities.X = -2;
+                boi.velocities.Y = 0;
             }
+            if (kb.IsKeyDown(Keys.D))
+            {
+                boi.velocities.X = 2;
+                boi.velocities.Y = 0;
+            }
+            if (kb.IsKeyDown(Keys.W))
+            {
+                boi.velocities.Y = -2;
+                boi.velocities.X = 0;
+            }
+            if (kb.IsKeyDown(Keys.S))
+            {
+                boi.velocities.Y = 2;
+                boi.velocities.X = 0;
+            }
+
+            boi.Update();
             base.Update(gameTime);
         }
 
@@ -124,6 +142,7 @@ namespace Pacman
 
             spriteBatch.Begin();
             spriteBatch.Draw(boardt, boardr, Color.White);
+            spriteBatch.Draw(boi.tex, boi.rec, boi.source, boi.colour);
             spriteBatch.End();
             // TODO: Add your drawing code here
 
--- PacMan/PacMan/PacMan/Pacboi.cs
+++ PacMan/PacMan/PacMan/Pacboi.cs
@@ -13,7 +13,7 @@ namespace PacMan
 {
     public class Pacboi
     {
-        public Texture2D puck;
+        public Texture2D tex;
         public Rectangle rec;
         public Rectangle source;
         public Vector2 velocities = new Vector2(0, 0);
@@ -24,11 +24,33 @@ namespace PacMan
         }
         public Pacboi(Texture2D t, Rectangle r, Rectangle s, Vector2 vel)
         {
-            puck = t;
+            tex = t;
             rec = r;
             source = s;
             velocities = vel;
             colour = Color.White;
+        }
+        public void Update()
+        {
+            if (velocities.Y > 0)
+            {
+                source.Y = 48;
+            }
+            if (velocities.Y < 0)
+            {
+                source.Y = 32;
+            }
+            if (velocities.X > 0)
+            {
+                source.Y = 0;
+            }
+            if (velocities.X < 0)
+            {
+                source.Y = 16;
+            }
+            rec.X += (int) velocities.X;
+            rec.Y += (int) velocities.Y;
+
         }
     }
 }
